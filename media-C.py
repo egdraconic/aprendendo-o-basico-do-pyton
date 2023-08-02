8e75d9cddafa12efe4586fd26cec48a16dc6f48f
egdraconic <lucasrogerio9898>
2023-08-02 20:26:11 -0300

    criado para calcular a media de um circulo

---
+++ media-C.py
@@ -0,0 +1,6 @@
+numero1=int(input("digiti um valor ate o numero trinta:"))
+numero2=int(input("digite novamente outro valor ate trinta"))
+numero3=int(input("digite o ultimo valor ate trinta"))
+
+media=(numero1+numero2+numero3) /3
+print(media)
